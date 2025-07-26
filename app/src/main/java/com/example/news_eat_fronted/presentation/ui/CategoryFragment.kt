92177d3aed49e4c9b4875e720243a468f39063f3
younheejae <hjy10045@sookmyung.ac.kr>
2025-07-27 06:11:41 +0900

    feat: #1 바텀네비게이션 구현 및 프래그먼트 연결

---
+++ app/src/main/java/com/example/news_eat_fronted/presentation/ui/CategoryFragment.kt
@@ -0,0 +1,10 @@
+package com.example.news_eat_fronted.presentation.ui
+
+import com.example.news_eat_fronted.R
+import com.example.news_eat_fronted.databinding.FragmentCategoryBinding
+import com.example.news_eat_fronted.databinding.FragmentHomeBinding
+import com.example.news_eat_fronted.util.base.BindingFragment
+
+class CategoryFragment: BindingFragment<FragmentCategoryBinding>(R.layout.fragment_category) {
+
+}
---
+++ app/src/main/java/com/example/news_eat_fronted/presentation/ui/MyPageFragment.kt
@@ -0,0 +1,9 @@
+package com.example.news_eat_fronted.presentation.ui
+
+import com.example.news_eat_fronted.R
+import com.example.news_eat_fronted.databinding.FragmentHomeBinding
+import com.example.news_eat_fronted.databinding.FragmentMypageBinding
+import com.example.news_eat_fronted.util.base.BindingFragment
+
+class MyPageFragment: BindingFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
+}
