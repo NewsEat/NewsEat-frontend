f701b21f78dee30b0e7ecddebdadef52c3fa9ee2
jiwonniy <jiwoni0707@gmail.com>
2025-08-06 15:37:28 +0900

    refactor: #3 화면 이동 트리거 역할 코드 제거

--- app/src/main/java/com/example/news_eat_fronted/presentation/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/example/news_eat_fronted/presentation/ui/mypage/MyPageViewModel.kt
@@ -1,12 +1,8 @@
 package com.example.news_eat_fronted.presentation.ui.mypage
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 class MyPageViewModel : ViewModel() {
 
@@ -16,37 +12,4 @@ class MyPageViewModel : ViewModel() {
     private val _interests = MutableStateFlow<List<String>>(listOf("연예", "IT/과학", "세계"))
     val interests: StateFlow<List<String>> = _interests
 
-    private val _navigateTo = MutableSharedFlow<MyPageNavigation>()
-    val navigateTo: SharedFlow<MyPageNavigation> = _navigateTo
-
-    fun onClickEditInterest() {
-        viewModelScope.launch {
-            _navigateTo.emit(MyPageNavigation.GoToEditInterest)
-        }
-    }
-
-    fun onClickEditProfile() {
-        viewModelScope.launch {
-            _navigateTo.emit(MyPageNavigation.GoToEditProfile)
-        }
-    }
-
-    fun onClickLogout() {
-        viewModelScope.launch {
-            _navigateTo.emit(MyPageNavigation.Logout)
-        }
-    }
-
-    fun onClickWithdraw() {
-        viewModelScope.launch {
-            _navigateTo.emit(MyPageNavigation.Withdraw)
-        }
-    }
-
-    sealed class MyPageNavigation {
-        object GoToEditInterest : MyPageNavigation()
-        object GoToEditProfile : MyPageNavigation()
-        object Logout : MyPageNavigation()
-        object Withdraw : MyPageNavigation()
-    }
 }
