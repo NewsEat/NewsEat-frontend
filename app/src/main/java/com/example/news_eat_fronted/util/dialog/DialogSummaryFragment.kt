ee342d3cc84752cecd7efb3a693aa370fd78eca8
younheejae <hjy10045@sookmyung.ac.kr>
2025-08-13 23:01:47 +0900

    feat: #13 뉴스요약 다이얼로그 구현 및 연결

--- app/src/main/java/com/example/news_eat_fronted/util/dialog/DialogSummaryFragment.kt
+++ app/src/main/java/com/example/news_eat_fronted/util/dialog/DialogSummaryFragment.kt
@@ -4,18 +4,15 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import com.example.news_eat_fronted.R
-import com.example.news_eat_fronted.databinding.DialogPopupBinding
+import com.example.news_eat_fronted.databinding.DialogSummaryBinding
 import com.example.news_eat_fronted.util.base.BindingDialogFragment
 
-class DialogPopupFragment(
+class DialogSummaryFragment(
     private val title: String,
     private val content: String,
-    private val leftBtnText: String,
-    private val rightBtnText: String,
-    private val clickLeftBtn: () -> Unit,
-    private val clickRightBtn: () -> Unit,
+    private val tag: String,
     private val closeDialog: () -> Unit = {},
-): BindingDialogFragment<DialogPopupBinding>(R.layout.dialog_popup) {
+): BindingDialogFragment<DialogSummaryBinding>(R.layout.dialog_summary) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -30,25 +27,18 @@ class DialogPopupFragment(
 
     private fun initLayout() {
         with(binding) {
-            dialogTitle.text = title
-            dialogContent.text = content
-            btnCancel.text = leftBtnText
-            btnConfirm.text = rightBtnText
+            summaryTitle.text = "\"$title\""
+            summaryContent.text = content
+            summaryTag.text = tag
         }
     }
 
     private fun addListeners() {
-        binding.btnCancel.setOnClickListener {
-            clickLeftBtn()
-            dismiss()
-        }
-
-        binding.btnConfirm.setOnClickListener {
-            clickRightBtn()
-            dismiss()
-        }
-
         binding.dialogBackground.setOnClickListener {
+            dismiss()
+        }
+
+        binding.btnBack.setOnClickListener {
             dismiss()
         }
     }
